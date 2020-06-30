427258f463238c9e4abcd84b4399403c8a0954bf
Saymon85 <saymon_bg@hotmail.com>
2020-06-30 23:57:31 +0200

    small refactor of modal.js

--- scritps/modal.js
+++ scritps/modal.js
@@ -2,7 +2,6 @@ const expandWorkItem = document.querySelectorAll('.work-item__expand');
 const sliderArrowLeft = document.querySelector('.slider-arrow--left');
 const sliderArrowRight = document.querySelector('.slider-arrow--right');
 const closeButton = document.querySelector('.modal-close-btn');
-expandWorkItem.forEach((item) => item.addEventListener('click', openModal));
 let sliderOffset = 0;
 
 function openModal(e) {
@@ -12,14 +11,13 @@ function openModal(e) {
   console.log(sliderOffset);
   sliderOffset -= modalItem[e.target.id].offsetLeft;
   slider.style.transform = `translateX(${sliderOffset}px)`;
-  setTimeout(() => modal.classList.add('modal--active'), 300);
+  modal.classList.add('modal--active');
+}
 
-  /*console.log(modalItem[0].offsetLeft);
-  console.log(modalItem[1].offsetLeft);
-  console.log(modalItem[2].offsetLeft);
-  console.log(modalItem[3].offsetLeft);
-  console.log(modalItem[4].offsetLeft);
-  console.log(modalItem[5].offsetLeft); */
+function closeModal() {
+  const modal = document.querySelector('.modal');
+  modal.classList.remove('modal--active');
+  sliderOffset = 0;
 }
 
 sliderArrowRight.addEventListener('click', (e) => {
@@ -28,25 +26,18 @@ sliderArrowRight.addEventListener('click', (e) => {
   sliderOffset -= slider.offsetWidth;
   slider.style.transform = `translateX(${sliderOffset}px)`;
   if (sliderOffset == 5 * -slider.offsetWidth) {
-    e.target.parentElement.style.pointerEvents = 'none';
-    e.target.style.opacity = '0.3';
+    sliderArrowRight.style.pointerEvents = 'none';
+    sliderArrowRight.style.opacity = '0.3';
   }
 });
 
 sliderArrowLeft.addEventListener('click', (e) => {
   const slider = document.querySelector('.modal-slider');
+  if (sliderOffset == 0) return;
   slider.style.transform = 'translateX(0px)';
   sliderOffset = 0;
-  sliderArrowRight.firstElementChild.style.opacity = '1';
+  sliderArrowRight.style.opacity = '1';
   sliderArrowRight.style.pointerEvents = 'auto';
 });
-
-/* function closeModal(modal) {
-  
-} */
-
-closeButton.addEventListener('click', () => {
-  const modal = document.querySelector('.modal');
-  modal.classList.remove('modal--active');
-  sliderOffset = 0;
-});
+expandWorkItem.forEach((item) => item.addEventListener('click', openModal));
+closeButton.addEventListener('click', closeModal);
