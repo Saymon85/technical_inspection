292d14d34b52c2ebe985b51fbb2805947a96ba2a
Saymon85 <saymon_bg@hotmail.com>
2020-07-02 19:42:13 +0200

    added GSAP animation library, working on modal and slider in modal

--- scritps/modal.js
+++ scritps/modal.js
@@ -7,37 +7,70 @@ let sliderOffset = 0;
 function openModal(e) {
   const modalItem = document.querySelectorAll('.modal-item');
   const slider = document.querySelector('.modal-slider');
-  const modal = document.querySelector('.modal');
-  console.log(sliderOffset);
+
   sliderOffset -= modalItem[e.target.id].offsetLeft;
   slider.style.transform = `translateX(${sliderOffset}px)`;
-  modal.classList.add('modal--active');
+  gsap.to('.modal', {
+    transform: 'scale(1,1)',
+    duration: 0.8,
+    ease: 'power2.inOut',
+  });
 }
 
 function closeModal() {
-  const modal = document.querySelector('.modal');
-  modal.classList.remove('modal--active');
+  gsap.to('.modal', {
+    transform: 'scale(0,0)',
+    duration: 0.4,
+    ease: 'power4',
+  });
   sliderOffset = 0;
+  sliderArrowLeft.classList.remove('slider-arrow--disabled');
+  sliderArrowRight.classList.remove('slider-arrow--disabled');
 }
 
 sliderArrowRight.addEventListener('click', (e) => {
   const slider = document.querySelector('.modal-slider');
-  //const modalwrap = document.querySelector('.modal-wrap');
   sliderOffset -= slider.offsetWidth;
-  slider.style.transform = `translateX(${sliderOffset}px)`;
-  if (sliderOffset == 5 * -slider.offsetWidth) {
-    sliderArrowRight.style.pointerEvents = 'none';
-    sliderArrowRight.style.opacity = '0.3';
+
+  if (sliderOffset <= 5 * -slider.offsetWidth) {
+    sliderArrowRight.classList.add('slider-arrow--disabled');
+    gsap.to('.modal-slider', {
+      transform: `translateX(${sliderOffset}px)`,
+      duration: 1,
+      delay: 0.1,
+      ease: 'back',
+    });
+  } else {
+    gsap.to('.modal-slider', {
+      transform: `translateX(${sliderOffset}px)`,
+      duration: 0.8,
+      ease: 'sine',
+    });
   }
+  sliderArrowLeft.classList.remove('slider-arrow--disabled');
 });
 
 sliderArrowLeft.addEventListener('click', (e) => {
   const slider = document.querySelector('.modal-slider');
-  if (sliderOffset == 0) return;
-  slider.style.transform = 'translateX(0px)';
-  sliderOffset = 0;
-  sliderArrowRight.style.opacity = '1';
-  sliderArrowRight.style.pointerEvents = 'auto';
+  sliderOffset += slider.offsetWidth;
+  if (sliderOffset > -slider.offsetWidth) {
+    gsap.to('.modal-slider', {
+      transform: `translateX(0px)`,
+      duration: 1,
+      delay: 0.1,
+      ease: 'back',
+    });
+    sliderArrowLeft.classList.add('slider-arrow--disabled');
+    sliderOffset = 0;
+  } else {
+    gsap.to('.modal-slider', {
+      transform: `translateX(${sliderOffset}px)`,
+      duration: 0.8,
+      ease: 'sine',
+    });
+  }
+  sliderArrowRight.classList.remove('slider-arrow--disabled');
 });
+
 expandWorkItem.forEach((item) => item.addEventListener('click', openModal));
 closeButton.addEventListener('click', closeModal);
