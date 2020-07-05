bb7a03da0d23325cc156adbda823706a13a5140d
Saymon85 <saymon_bg@hotmail.com>
2020-07-05 18:39:02 +0200

    change of font for entire project

--- scritps/modal.js
+++ scritps/modal.js
@@ -5,14 +5,16 @@ const closeButton = document.querySelector('.modal-close-btn');
 let sliderOffset = 0;
 
 function openModal(e) {
-  if (document.body.offsetWidth < 800) return; // don't open modal for mobile phones
-  document.body.addEventListener('keyup', (e) => {
-    if (e.code === 'ArrowRight') moveSlideRight();
-    if (e.code === 'ArrowLeft') moveSlideLeft();
-    if (e.code === 'Escape') closeModal();
-  });
   const slider = document.querySelector('.modal-slider');
   sliderOffset = -e.target.id * slider.offsetWidth;
+
+  if (document.body.offsetWidth < 800) return; // don't open modal for display width lesser than 800px
+  if (sliderOffset === 0)
+    sliderArrowLeft.classList.add('slider-arrow--disabled');
+  if (sliderOffset === -5 * slider.offsetWidth)
+    sliderArrowRight.classList.add('slider-arrow--disabled');
+  document.body.addEventListener('keyup', attachKeyboardControls); // Add keyboard controls for slider
+
   gsap.to('.modal-slider', {
     transform: `translateX(${sliderOffset}px)`,
     duration: 0.1,
@@ -33,7 +35,7 @@ function closeModal() {
   sliderOffset = 0;
   sliderArrowLeft.classList.remove('slider-arrow--disabled');
   sliderArrowRight.classList.remove('slider-arrow--disabled');
-  document.body.removeEventListener('keyup', () => '');
+  document.body.removeEventListener('keyup', attachKeyboardControls);
 }
 
 function moveSlideRight() {
@@ -41,13 +43,13 @@ function moveSlideRight() {
   sliderOffset -= slider.offsetWidth;
 
   if (sliderOffset <= 5 * -slider.offsetWidth) {
-    sliderArrowRight.classList.add('slider-arrow--disabled');
     gsap.to('.modal-slider', {
       transform: `translateX(${sliderOffset}px)`,
       duration: 1.2,
       delay: 0.1,
       ease: 'back',
     });
+    sliderArrowRight.classList.add('slider-arrow--disabled');
   } else {
     gsap.to('.modal-slider', {
       transform: `translateX(${sliderOffset}px)`,
@@ -78,6 +80,12 @@ function moveSlideLeft() {
     });
   }
   sliderArrowRight.classList.remove('slider-arrow--disabled');
+}
+
+function attachKeyboardControls(e) {
+  if (e.code === 'ArrowRight') moveSlideRight();
+  if (e.code === 'ArrowLeft') moveSlideLeft();
+  if (e.code === 'Escape') closeModal();
 }
 
 expandWorkItem.forEach((item) => item.addEventListener('click', openModal));
