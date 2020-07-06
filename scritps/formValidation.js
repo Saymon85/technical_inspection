c1991ab5ed3f07e61cec5903655db438830bcbde
Saymon85 <saymon_bg@hotmail.com>
2020-07-06 20:59:09 +0200

    minor refactor

--- scritps/formValidation.js
+++ scritps/formValidation.js
@@ -1,57 +1,75 @@
-const form = document.querySelector('form');
-const inputName = document.querySelector('#name');
-const inputEmail = document.querySelector('#email');
-const inputSubject = document.querySelector('#subject');
-const textArea = document.querySelector('textarea');
+function formValidation() {
+  // Isolating from global namespace
+  const inputName = document.querySelector('#name');
+  const inputEmail = document.querySelector('#email');
+  const inputSubject = document.querySelector('#subject');
+  const textArea = document.querySelector('textarea');
+  const sendButton = document.querySelector('.btn-submit');
 
-function validateNameField() {
-  const regex = /\d+|\W+/g;
+  function validateNameField() {
+    const regex = /\d+|\W+/g;
 
-  if (regex.test(inputName.value)) {
-    inputName.setCustomValidity('Ime moze sadrzati samo slova i donju crtu');
-    inputName.classList.add('input--invalid');
-  } else if (inputName.validity.tooShort) {
-    inputName.setCustomValidity('Ime mora sadrzati minimum cetiri slova');
-    inputName.classList.add('input--invalid');
-  } else if (!inputName.validity.valid) {
-    inputName.setCustomValidity('Morate uneti Vase ime');
-    inputName.classList.add('input--invalid');
-  } else {
-    inputName.setCustomValidity('');
-    inputName.classList.remove('input--invalid');
-  }
-}
-
-function validateEmailFiled() {
-  if (inputEmail.validity.typeMismatch) {
-    inputEmail.setCustomValidity(
-      'Morate uneti email adresu u formi xxx@abc.xy'
-    );
-    inputEmail.classList.add('input--invalid');
-  } else if (!inputEmail.validity.valid) {
-    inputEmail.setCustomValidity('Polje za email je obavezno');
-    inputEmail.classList.add('input--invalid');
-  } else {
-    inputEmail.setCustomValidity('');
-    inputEmail.classList.remove('input--invalid');
-  }
-}
-
-function validateSubjectField(e) {
-  if (e.target.validity.valueMissing) {
-    if (e.target.classList.contains('textarea')) {
-      e.target.setCustomValidity('Molimo vas unesite poruku');
+    if (regex.test(inputName.value)) {
+      inputName.setCustomValidity('Ime moze sadrzati samo slova i donju crtu');
+      inputName.classList.add('input--invalid');
+    } else if (inputName.validity.tooShort) {
+      inputName.setCustomValidity('Ime mora sadrzati minimum cetiri slova');
+      inputName.classList.add('input--invalid');
+    } else if (inputName.validity.valueMissing) {
+      inputName.setCustomValidity('Morate uneti Vase ime');
+      inputName.classList.add('input--invalid');
     } else {
-      e.target.setCustomValidity('Naslov poruke je obavezan');
+      inputName.setCustomValidity('');
+      inputName.classList.remove('input--invalid');
     }
-    e.target.classList.add('input--invalid');
-  } else {
-    e.target.setCustomValidity('');
-    e.target.classList.remove('input--invalid');
   }
+
+  function validateEmailFiled() {
+    if (inputEmail.validity.typeMismatch) {
+      inputEmail.setCustomValidity(
+        'Morate uneti email adresu u formi xxx@abc.xy'
+      );
+      inputEmail.classList.add('input--invalid');
+    } else if (!inputEmail.validity.valid) {
+      inputEmail.setCustomValidity('Polje za email je obavezno');
+      inputEmail.classList.add('input--invalid');
+    } else {
+      inputEmail.setCustomValidity('');
+      inputEmail.classList.remove('input--invalid');
+    }
+  }
+
+  function validateSubjectField(e) {
+    if (inputSubject.validity.valueMissing) {
+      inputSubject.setCustomValidity('Naslov poruke je obavezan');
+      inputSubject.classList.add('input--invalid');
+    } else {
+      inputSubject.setCustomValidity('');
+      inputSubject.classList.remove('input--invalid');
+    }
+  }
+
+  function validateTextareaField(e) {
+    if (textArea.validity.valueMissing) {
+      textArea.setCustomValidity('Molimo vas unesite poruku');
+      textArea.classList.add('input--invalid');
+    } else {
+      textArea.setCustomValidity('');
+      textArea.classList.remove('input--invalid');
+    }
+  }
+
+  sendButton.addEventListener('click', () => {
+    validateNameField();
+    validateEmailFiled();
+    validateSubjectField();
+    validateTextareaField();
+  });
+
+  inputName.addEventListener('blur', validateNameField);
+  inputEmail.addEventListener('blur', validateEmailFiled);
+  inputSubject.addEventListener('blur', validateSubjectField);
+  textArea.addEventListener('blur', validateTextareaField);
 }
 
-inputName.addEventListener('blur', validateNameField);
-inputEmail.addEventListener('blur', validateEmailFiled);
-inputSubject.addEventListener('blur', validateSubjectField);
-textArea.addEventListener('blur', validateSubjectField);
+formValidation();
--- scritps/modal.js
+++ scritps/modal.js
@@ -1,94 +1,99 @@
-const expandWorkItem = document.querySelectorAll('.work-item__expand');
-const sliderArrowLeft = document.querySelector('.slider-arrow--left');
-const sliderArrowRight = document.querySelector('.slider-arrow--right');
-const closeButton = document.querySelector('.modal-close-btn');
-let sliderOffset = 0;
+function modal() {
+  // Isolating from global namespace
+  const expandWorkItem = document.querySelectorAll('.work-item__expand');
+  const sliderArrowLeft = document.querySelector('.slider-arrow--left');
+  const sliderArrowRight = document.querySelector('.slider-arrow--right');
+  const closeButton = document.querySelector('.modal-close-btn');
+  let sliderOffset = 0;
 
-function openModal(e) {
-  const slider = document.querySelector('.modal-slider');
-  sliderOffset = -e.target.id * slider.offsetWidth;
+  function openModal(e) {
+    const slider = document.querySelector('.modal-slider');
+    sliderOffset = -e.target.id * slider.offsetWidth;
 
-  if (document.body.offsetWidth < 800) return; // don't open modal for display width lesser than 800px
-  if (sliderOffset === 0)
-    sliderArrowLeft.classList.add('slider-arrow--disabled');
-  if (sliderOffset === -5 * slider.offsetWidth)
-    sliderArrowRight.classList.add('slider-arrow--disabled');
-  document.body.addEventListener('keyup', attachKeyboardControls); // Add keyboard controls for slider
+    if (document.body.offsetWidth < 800) return; // don't open modal for display width lesser than 800px
+    if (sliderOffset === 0)
+      sliderArrowLeft.classList.add('slider-arrow--disabled');
+    if (sliderOffset === -5 * slider.offsetWidth)
+      sliderArrowRight.classList.add('slider-arrow--disabled');
+    document.body.addEventListener('keyup', attachKeyboardControls); // Add keyboard controls for slider
 
-  gsap.to('.modal-slider', {
-    transform: `translateX(${sliderOffset}px)`,
-    duration: 0.1,
-  });
-  gsap.to('.modal', {
-    transform: 'scale(1,1)',
-    duration: 0.8,
-    ease: 'power2.inOut',
-  });
-}
-
-function closeModal() {
-  gsap.to('.modal', {
-    transform: 'scale(0,0)',
-    duration: 0.4,
-    ease: 'power4',
-  });
-  sliderOffset = 0;
-  sliderArrowLeft.classList.remove('slider-arrow--disabled');
-  sliderArrowRight.classList.remove('slider-arrow--disabled');
-  document.body.removeEventListener('keyup', attachKeyboardControls);
-}
-
-function moveSlideRight() {
-  const slider = document.querySelector('.modal-slider');
-  sliderOffset -= slider.offsetWidth;
-
-  if (sliderOffset <= 5 * -slider.offsetWidth) {
     gsap.to('.modal-slider', {
       transform: `translateX(${sliderOffset}px)`,
-      duration: 1.2,
-      delay: 0.1,
-      ease: 'back',
+      duration: 0.1,
     });
-    sliderArrowRight.classList.add('slider-arrow--disabled');
-  } else {
-    gsap.to('.modal-slider', {
-      transform: `translateX(${sliderOffset}px)`,
+    gsap.to('.modal', {
+      transform: 'scale(1,1)',
       duration: 0.8,
-      ease: 'sine',
+      ease: 'power2.inOut',
     });
   }
-  sliderArrowLeft.classList.remove('slider-arrow--disabled');
-}
 
-function moveSlideLeft() {
-  const slider = document.querySelector('.modal-slider');
-  sliderOffset += slider.offsetWidth;
-  if (sliderOffset > -slider.offsetWidth) {
-    gsap.to('.modal-slider', {
-      transform: `translateX(0px)`,
-      duration: 1.2,
-      delay: 0.1,
-      ease: 'back',
+  function closeModal() {
+    gsap.to('.modal', {
+      transform: 'scale(0,0)',
+      duration: 0.4,
+      ease: 'power4',
     });
-    sliderArrowLeft.classList.add('slider-arrow--disabled');
     sliderOffset = 0;
-  } else {
-    gsap.to('.modal-slider', {
-      transform: `translateX(${sliderOffset}px)`,
-      duration: 0.8,
-      ease: 'sine',
-    });
+    sliderArrowLeft.classList.remove('slider-arrow--disabled');
+    sliderArrowRight.classList.remove('slider-arrow--disabled');
+    document.body.removeEventListener('keyup', attachKeyboardControls);
   }
-  sliderArrowRight.classList.remove('slider-arrow--disabled');
+
+  function moveSlideRight() {
+    const slider = document.querySelector('.modal-slider');
+    sliderOffset -= slider.offsetWidth;
+
+    if (sliderOffset <= 5 * -slider.offsetWidth) {
+      gsap.to('.modal-slider', {
+        transform: `translateX(${sliderOffset}px)`,
+        duration: 1.2,
+        delay: 0.1,
+        ease: 'back',
+      });
+      sliderArrowRight.classList.add('slider-arrow--disabled');
+    } else {
+      gsap.to('.modal-slider', {
+        transform: `translateX(${sliderOffset}px)`,
+        duration: 0.8,
+        ease: 'sine',
+      });
+    }
+    sliderArrowLeft.classList.remove('slider-arrow--disabled');
+  }
+
+  function moveSlideLeft() {
+    const slider = document.querySelector('.modal-slider');
+    sliderOffset += slider.offsetWidth;
+    if (sliderOffset > -slider.offsetWidth) {
+      gsap.to('.modal-slider', {
+        transform: `translateX(0px)`,
+        duration: 1.2,
+        delay: 0.1,
+        ease: 'back',
+      });
+      sliderArrowLeft.classList.add('slider-arrow--disabled');
+      sliderOffset = 0;
+    } else {
+      gsap.to('.modal-slider', {
+        transform: `translateX(${sliderOffset}px)`,
+        duration: 0.8,
+        ease: 'sine',
+      });
+    }
+    sliderArrowRight.classList.remove('slider-arrow--disabled');
+  }
+
+  function attachKeyboardControls(e) {
+    if (e.code === 'ArrowRight') moveSlideRight();
+    if (e.code === 'ArrowLeft') moveSlideLeft();
+    if (e.code === 'Escape') closeModal();
+  }
+
+  expandWorkItem.forEach((item) => item.addEventListener('click', openModal));
+  closeButton.addEventListener('click', closeModal);
+  sliderArrowRight.addEventListener('click', moveSlideRight);
+  sliderArrowLeft.addEventListener('click', moveSlideLeft);
 }
 
-function attachKeyboardControls(e) {
-  if (e.code === 'ArrowRight') moveSlideRight();
-  if (e.code === 'ArrowLeft') moveSlideLeft();
-  if (e.code === 'Escape') closeModal();
-}
-
-expandWorkItem.forEach((item) => item.addEventListener('click', openModal));
-closeButton.addEventListener('click', closeModal);
-sliderArrowRight.addEventListener('click', moveSlideRight);
-sliderArrowLeft.addEventListener('click', moveSlideLeft);
+modal();
